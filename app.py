2a1af2ccdb945d7251a6a05ba64851180fac6d66
Kyle Su <kylesu@macbook-pro.attlocal.net>
2018-01-05 18:25:20 -0800

    completed WebApp

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from sudoku import *
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,10 +8,11 @@ def main():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+	entries = [request.form[str(x)] for x in range(1, 82)]
+	game = [0 if x == "" else int(x) for x in entries]
+	sudoku_game = Game(game)
+	solved = sudoku_game.solve()
+	return render_template('solution.html', solution=solved)
 
 if __name__ == "__main__":
     app.run()
